162196e96b891a038ae550da4bc044793f1bf4c5
codejapoe <codejapoe@gmail.com>
2025-04-05 10:24:29 +0000

    fixed minor issue

--- flutter-app/test/widget_test.dart
+++ flutter-app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:robot_control_center/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const EmoRoboApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
